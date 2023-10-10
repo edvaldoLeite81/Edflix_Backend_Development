8e1721502da5cad76040c5f327f2d8b73905ed65
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-10-10 11:54:06 -0300

    update

--- src/main/java/com/edflix/controller/PaperController.java
+++ src/main/java/com/edflix/controller/PaperController.java
@@ -1,17 +1,13 @@
 package com.edflix.controller;
 
 import com.edflix.entities.Paper;
-import com.edflix.entities.pk.PaperPK;
 import com.edflix.repositories.PaperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/papers")
